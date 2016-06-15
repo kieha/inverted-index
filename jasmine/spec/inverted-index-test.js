a7857a6744a5832d121beb3e079141f5eae626f7
Sylvia Kieha <sylvia.kieha@andela.com>
2016-06-15 12:06:04 +0300

    createIndex function and test

--- jasmine/spec/inverted-index-test.js
+++ jasmine/spec/inverted-index-test.js
@@ -1,10 +1,22 @@
-// asserting book.json is not empty
-var obj = new Index();
-obj.createIndex('../../books.json');
-var contents = obj.fileContents;
+describe("Inverted Index Tests", function () {
+  var index = new Index();
 
-describe("Read book data", function () {
-  it("Checks to assert that book.json is not empty", function () {
-    expect(contents.length).not.toBe(0);
+// perform this before the start of each test
+  beforeEach(function(done) {
+    index.createIndex('/books.json').then(function(data) {
+      index.results = data;
+      done();
+    });
+  });
+
+  describe("Read book data", function() {
+    it("Checks to assert that book.json is not empty", function () {
+      expect(typeof index.results).toBe('object');
+      expect(index.results.length).not.toEqual(0);
+    });
+
+    it("should check if input file is a valid json object", function () {
+      expect(index.fileContents).not.toBeUndefined();
+    });
   });
 });
--- src/inverted-index.js
+++ src/inverted-index.js
@@ -1,49 +1,23 @@
 // Index object
 function Index() {
-  var book = [
-    {
-      "title": "Alice in Wonderland",
-      "text": "Alice falls into a rabbit hole and enters a world full of imagination."
-    },
+  this.fileContents = undefined;
+  // this.index_ = {};
+  var self = this;
 
-    {
-      "title": "The Lord of the Rings: The Fellowship of the Ring.",
-      "text": "An unusual alliance of man, elf, dwarf, wizard and hobbit seek to destroy a powerful ring."
-    }
-  ];
-
-  this.fileContents = [];
-  this.index_ = {};
-  // this.createIndex = function(filepath) {
-  //   fetch(filepath)
-  //     .then(function(response) {
-  //       response.json();
-  //       return response.json();
-  //     });
-  // };
-
-  this.getIndex = function() {
-     var self = this;
-     for (var i = 0; i < book.length; i++) {
-       var obj = book[i];
-       Object.keys(obj).map(function(key) {
-         self.fileContents.push(obj[key]);
-       });
-       strFileContents = this.fileContents.toString().toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]+/gi, " ").replace(/\s{2,}/g, " ").trim().split(" ");
-      //  console.log(i);
-       for (var x = 0; x < strFileContents.length; x++) {
-         this.index_[strFileContents[x]] = i;
-
-       }
-       console.log(this.index_);
-     }
-    //  console.log(this.index_);
-    //  strFileContents = this.fileContents.toString().toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]+/gi, " ").replace(/\s{2,}/g, " ").trim().split(" ");
-
-    //  var stopWords = ["of", "the", "a", "an", "and", "but", "to", "for"];
-    //  reg = "\\b" + stopWords.toString().replace(/,/g, "\\b|\\b") + "\\b";
+  this.createIndex = function(filepath) {
+    return fetch(filepath)
+      .then(function(response) {
+        return response.text();
+      }).then(function(response) {
+        try {
+          self.fileContents = JSON.parse(response);
+        } catch (e) {
+          console.log(e);
+        }
+        return self.fileContents;
+      }).catch(function(err) {
+        console.log('parsing failed', err);
+        throw err;
+      });
   };
 }
-
-var newind = new Index();
-newind.getIndex();

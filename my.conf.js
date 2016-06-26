9e18d357b0491ec55a97c21d5244beb7e98a8475
Sylvia Kieha <sylvia.kieha@andela.com>
2016-06-27 00:16:40 +0300

    my.conf.js edit

--- my.conf.js
+++ my.conf.js
@@ -15,7 +15,7 @@ module.exports = function (config) {
 
     // list of files / patterns to load in the browser
     files: [
-      '*.json',
+      'books.json',
       'jasmine/spec/*.js',
       'src/*.js'
     ],

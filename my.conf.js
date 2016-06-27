e079cb7440ed28f3a543d25078c9b30a5df2d8ca
Sylvia Kieha <sylvia.kieha@andela.com>
2016-06-27 09:58:42 +0300

    karma integration

--- my.conf.js
+++ my.conf.js
@@ -15,9 +15,9 @@ module.exports = function (config) {
 
     // list of files / patterns to load in the browser
     files: [
+      './src/*.js',
       './books.json',
-      './jasmine/spec/*.js',
-      './src/*.js'
+      './jasmine/spec/*.js'
     ],
 
 
